2c6e12b2ba3b074e1a592a3525fc1b4e4a50b8ea
Royette Millares <royette@spectorandco.com>
2024-01-22 04:09:09 +0800

    adjust css

--- components/Home/hero.tsx
+++ components/Home/hero.tsx
@@ -4,8 +4,8 @@ import DynamicTitle from "../DynamicTitle"
 import useScrollToBottom from "@/utils"
 export default function Hero() {
     return (
-        <section className="p-4 mx-auto max-w-screen-xl h-full w-full mb-[5rem]">
-            <div className="grid grid-cols-1 md:grid-cols-2 items-center">
+        <section className="p-4 mx-auto max-w-screen-xl h-full w-full mb-[5rem] xl:mt-[2.5rem]">
+            <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-5">
                 <div>
                     <h1 className="hero__title">
                         Find, book, rent a car—quick and
@@ -20,7 +20,7 @@ export default function Hero() {
                         Streamline your car rental experience with our effortless booking
                         process.
                     </p>
-                    <button className="p-4 mt-5 bg-blue-500 px-4 rounded-full hover:scale-105 transition-all text-white" onClick={useScrollToBottom}>Explore Cars</button>
+                    <button className="p-4 mt-5 text-blue-500 border-2 border-blue-500 cursor-pointer md:bg-blue-500 px-4 rounded-full hover:scale-105 transition-all md:text-white" onClick={useScrollToBottom}>Explore Cars</button>
                 </div>
                 <div>
                     <div className="hero__image-container relative overflow-hidden">
